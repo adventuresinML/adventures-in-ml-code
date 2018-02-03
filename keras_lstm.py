fc22a7bcba3b6d0133ddf90daa4fbe49aa4ca379
Andy <andy@adventuresinmachinelearning.com>
2018-02-03 12:18:58 +1000

    Finalised Keras LSTM tutorial code

--- keras_lstm.py
+++ keras_lstm.py
@@ -3,11 +3,12 @@ import collections
 import os
 import tensorflow as tf
 from keras.models import Sequential, load_model
-from keras.layers import Dense, Activation, Embedding, Flatten, Dropout, TimeDistributed, Reshape
+from keras.layers import Dense, Activation, Embedding, Flatten, Dropout, TimeDistributed, Reshape, Lambda
 from keras.layers import LSTM
 from keras.optimizers import RMSprop, Adam, SGD
 from keras import backend as K
 from keras.utils import to_categorical
+from keras.callbacks import ModelCheckpoint
 import numpy as np
 import argparse
 import pdb
@@ -72,10 +73,9 @@ train_data, valid_data, test_data, vocabulary, reversed_dictionary = load_data()
 
 class KerasBatchGenerator(object):
 
-    def __init__(self, data, num_steps, target_size, batch_size, vocabulary, skip_step=5):
+    def __init__(self, data, num_steps, batch_size, vocabulary, skip_step=5):
         self.data = data
         self.num_steps = num_steps
-        self.target_size = target_size
         self.batch_size = batch_size
         self.vocabulary = vocabulary
         # this will track the progress of the batches sequentially through the
@@ -88,82 +88,88 @@ class KerasBatchGenerator(object):
 
     def generate(self):
         x = np.zeros((self.batch_size, self.num_steps))
-        y = np.zeros((self.batch_size, self.target_size, self.vocabulary))
-        temp_y = np.zeros((self.batch_size, self.target_size))
-        # temp_y_2 = np.zeros((self.batch_size, self.target_size, self.vocabulary))
+        y = np.zeros((self.batch_size, self.num_steps, self.vocabulary))
         while True:
             for i in range(self.batch_size):
-                if self.current_idx + self.num_steps + self.target_size >= len(self.data):
+                if self.current_idx + self.num_steps >= len(self.data):
                     # reset the index back to the start of the data set
                     self.current_idx = 0
                 x[i, :] = self.data[self.current_idx:self.current_idx + self.num_steps]
-                temp_y[i, :] = self.data[self.current_idx + self.num_steps:self.current_idx
-                                                                           + self.num_steps + self.target_size]
+                temp_y = self.data[self.current_idx + 1:self.current_idx + self.num_steps + 1]
                 # convert all of temp_y into a one hot representation
-                # temp_y_2 = np.zeros((self.target_size, self.vocabulary))
-                # for j in range(self.target_size):
-                 #   temp_y_2[i, j, temp_y[i, j]] = 1
-                # now reshape to feed to softmax output layer, size = target_size * vocabulary
-                # y[i, :] = np.reshape(temp_y_2, (1, self.target_size * self.vocabulary))
-                y[i, :, :] = to_categorical(temp_y[i, :], num_classes=self.vocabulary)
-                # pdb.set_trace()
+                y[i, :, :] = to_categorical(temp_y, num_classes=self.vocabulary)
                 self.current_idx += self.skip_step
             yield x, y
 
-num_steps = 15
-target_size = 5
+num_steps = 30
 batch_size = 20
-train_data_generator = KerasBatchGenerator(train_data, num_steps, target_size, batch_size, vocabulary,
+train_data_generator = KerasBatchGenerator(train_data, num_steps, batch_size, vocabulary,
                                            skip_step=num_steps)
-valid_data_generator = KerasBatchGenerator(valid_data, num_steps, target_size, batch_size, vocabulary,
+valid_data_generator = KerasBatchGenerator(valid_data, num_steps, batch_size, vocabulary,
                                            skip_step=num_steps)
-test_data_generator = KerasBatchGenerator(test_data, num_steps, target_size, batch_size, vocabulary,
-                                          skip_step=num_steps)
 
-hidden_layers = 300
-
+hidden_size = 500
+use_dropout=True
 model = Sequential()
-model.add(Embedding(vocabulary, hidden_layers, input_length=num_steps))
-model.add(LSTM(hidden_layers, return_sequences=True))
-# model.add(LSTM(hidden_layers, return_sequences=True))
-model.add(Dropout(0.2))
-model.add(Reshape((target_size, -1)))
+model.add(Embedding(vocabulary, hidden_size, input_length=num_steps))
+model.add(LSTM(hidden_size, return_sequences=True))
+model.add(LSTM(hidden_size, return_sequences=True))
+if use_dropout:
+    model.add(Dropout(0.5))
 model.add(TimeDistributed(Dense(vocabulary)))
 model.add(Activation('softmax'))
-# model.add(Reshape((target_size*vocabulary,)))
 
-def our_accuracy(y_true, y_pred, target_size=10):
-    y_true = K.reshape(y_true, (target_size, -1))
-    y_pred = K.reshape(y_pred, (target_size, -1))
-    # pdb.set_trace()
-    return K.mean(K.equal(K.argmax(y_true, axis=-1), K.argmax(y_pred, axis=-1)))
-
-def loss(y_true, y_pred, target_size=10):
-    y_true = K.reshape(y_true, (target_size, -1))
-    y_pred = K.reshape(y_pred, (target_size, -1))
-
-
-# optimizer = RMSprop(lr=0.001)
-# optimizer = SGD(lr=1.0)
 optimizer = Adam()
-model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['categorical_accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['categorical_accuracy'])
 
 print(model.summary())
-
+checkpointer = ModelCheckpoint(filepath=data_path + '/model-{epoch:02d}.hdf5', verbose=1)
 num_epochs = 50
 if args.run_opt == 1:
     model.fit_generator(train_data_generator.generate(), len(train_data)//(batch_size*num_steps), num_epochs,
                         validation_data=valid_data_generator.generate(),
-                        validation_steps=len(valid_data)//(batch_size*num_steps))
+                        validation_steps=len(valid_data)//(batch_size*num_steps), callbacks=[checkpointer])
     # model.fit_generator(train_data_generator.generate(), 2000, num_epochs,
     #                     validation_data=valid_data_generator.generate(),
     #                     validation_steps=10)
-    model.save(data_path + "model.h5")
+    model.save(data_path + "final_model.hdf5")
 elif args.run_opt == 2:
-    model = load_model(data_path + "model.h5")
-    prediction = model.predict_generator(train_data_generator.generate(),steps=1)
-    pdb.set_trace()
-    x=1
+    model = load_model(data_path + "\model-40.hdf5")
+    dummy_iters = 40
+    example_training_generator = KerasBatchGenerator(train_data, num_steps, 1, vocabulary,
+                                                     skip_step=1)
+    print("Training data:")
+    for i in range(dummy_iters):
+        dummy = next(example_training_generator.generate())
+    num_predict = 10
+    true_print_out = "Actual words: "
+    pred_print_out = "Predicted words: "
+    for i in range(num_predict):
+        data = next(example_training_generator.generate())
+        prediction = model.predict(data[0])
+        predict_word = np.argmax(prediction[:, num_steps-1, :])
+        true_print_out += reversed_dictionary[train_data[num_steps + dummy_iters + i]] + " "
+        pred_print_out += reversed_dictionary[predict_word] + " "
+    print(true_print_out)
+    print(pred_print_out)
+    # test data set
+    dummy_iters = 40
+    example_test_generator = KerasBatchGenerator(test_data, num_steps, 1, vocabulary,
+                                                     skip_step=1)
+    print("Test data:")
+    for i in range(dummy_iters):
+        dummy = next(example_test_generator.generate())
+    num_predict = 10
+    true_print_out = "Actual words: "
+    pred_print_out = "Predicted words: "
+    for i in range(num_predict):
+        data = next(example_test_generator.generate())
+        prediction = model.predict(data[0])
+        predict_word = np.argmax(prediction[:, num_steps - 1, :])
+        true_print_out += reversed_dictionary[test_data[num_steps + dummy_iters + i]] + " "
+        pred_print_out += reversed_dictionary[predict_word] + " "
+    print(true_print_out)
+    print(pred_print_out)
 
 
 
